02766fb789b576b8cfe4ab23541f3e9743ccabca
griffinnS3 <160625385+griffinnS3@users.noreply.github.com>
2026-04-13 14:36:12 -0400

    Add files via upload

---
+++ Project_3/Project_3Tests/Project_3Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Project_3Tests.swift
+//  Project_3Tests
+//
+//  Created by Smith01, Griffin on 4/13/26.
+//
+
+import Testing
+@testable import Project_3
+
+struct Project_3Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
