6d6a56befc58dff63037276bac990ccec76d30d3
Eddie Slobodow <eddieslobodow@stu-6-144-171-184.champlain.edu>
2026-04-20 21:51:53 -0400

    Orthagonal Scroll View and cell

--- Project_3/Project_3/PokemonDetailView.swift
+++ Project_3/Project_3/PokemonDetailView.swift
@@ -19,6 +19,13 @@ class PokemonDetailView: UIViewController {
     let imageView = UIImageView()
     let nameLabel = UILabel()
     
+    enum Section {
+        case main
+    }
+
+    var dataSource: UICollectionViewDiffableDataSource<Section, Int>! = nil
+    var collectionView: UICollectionView! = nil
+    
     
     init(pokemon: PokemonEntry) {
         self.pokemon = pokemon
@@ -26,7 +33,7 @@ class PokemonDetailView: UIViewController {
         
     }
     override func viewDidLoad() {
-        view.backgroundColor = .systemBackground
+        /*view.backgroundColor = .systemBackground
         navigationItem.title = pokemon.name
         view.addSubview(imageView)
         view.addSubview(nameLabel)
@@ -35,7 +42,111 @@ class PokemonDetailView: UIViewController {
         if let spriteURL = pokemon.spriteURL {
             imageView.kf.setImage(with: spriteURL, placeholder: UIImage(systemName: "photo"),
                                   options: [.transition(.fade(0.3))])
-        }
+        }*/
+        
+        super.viewDidLoad()
+        navigationItem.title = "Nested Groups"
+        configureHierarchy()
+        configureDataSource()
     }
     
 }
+
+extension PokemonDetailView {
+
+    //   +-----------------------------------------------------+
+    //   | +---------------------------------+  +-----------+  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |     1     |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  +-----------+  |
+    //   | |               0                 |                 |
+    //   | |                                 |  +-----------+  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |     2     |  |
+    //   | |                                 |  |           |  |
+    //   | |                                 |  |           |  |
+    //   | +---------------------------------+  +-----------+  |
+    //   +-----------------------------------------------------+
+
+    /// - Tag: Nested
+    func createLayout() -> UICollectionViewLayout {
+        let layout = UICollectionViewCompositionalLayout {
+            (sectionIndex: Int, layoutEnvironment: NSCollectionLayoutEnvironment) -> NSCollectionLayoutSection? in
+
+            let leadingItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.7),
+                                                  heightDimension: .fractionalHeight(1.0)))
+            leadingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+
+            let trailingItem = NSCollectionLayoutItem(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(0.3)))
+            trailingItem.contentInsets = NSDirectionalEdgeInsets(top: 10, leading: 10, bottom: 10, trailing: 10)
+            let trailingGroup = NSCollectionLayoutGroup.vertical(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.3),
+                                                   heightDimension: .fractionalHeight(1.0)),
+                repeatingSubitem: trailingItem,
+                count: 4)
+            let nestedGroup = NSCollectionLayoutGroup.horizontal(
+                layoutSize: NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0),
+                                                  heightDimension: .fractionalHeight(0.4)),
+                subitems: [leadingItem, trailingGroup])
+            let section = NSCollectionLayoutSection(group: nestedGroup)
+            return section
+
+        }
+        return layout
+    }
+}
+
+extension PokemonDetailView {
+   func configureHierarchy() {
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createLayout())
+        collectionView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        collectionView.backgroundColor = .systemBackground
+        view.addSubview(collectionView)
+        collectionView.delegate = self
+    }
+    func configureDataSource() {
+        
+        let cellRegistration = UICollectionView.CellRegistration<TextCell, Int> { (cell, indexPath, identifier) in
+            // Populate the cell with our item description.
+            cell.label.text = "\(indexPath.section), \(indexPath.item)"
+            
+            // will have a case for each cell
+            /*switch indexPath.item {
+                case
+            }*/
+            
+            cell.contentView.backgroundColor = .blue
+            cell.contentView.layer.borderColor = UIColor.black.cgColor
+            cell.contentView.layer.borderWidth = 1
+            cell.contentView.layer.cornerRadius = 8
+            cell.label.textAlignment = .center
+            cell.label.font = UIFont.preferredFont(forTextStyle: .title1)
+        }
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Int>(collectionView: collectionView) {
+            (collectionView: UICollectionView, indexPath: IndexPath, identifier: Int) -> UICollectionViewCell? in
+            // Return the cell.
+            return collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: identifier)
+        }
+
+        // initial data
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Int>()
+        snapshot.appendSections([Section.main])
+        snapshot.appendItems(Array(0..<5))
+        dataSource.apply(snapshot, animatingDifferences: false)
+    }
+}
+
+extension PokemonDetailView: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+    }
+}
+
